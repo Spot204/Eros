dbbafc583a1a464e9828c05bad251eba93d7904d
HoaNgx-179 <hoahoa0917@gmail.com>
2025-12-10 01:03:43 +0700

    Create UserCardDTO.java

---
+++ backend/matchservice/src/main/java/com/eros/matchservice/dto/UserCardDTO.java
@@ -0,0 +1,16 @@
+package com.eros.matchservice.dto;
+
+import java.util.List;
+
+public class UserCardDTO {
+    public Long id;
+    public String name;
+    public Integer age;
+    public String bio;
+    public String education;
+    public String jobTitle;
+    public String company;
+    public String hobbies;
+    public String city;
+    public List<String> photos;
+}
