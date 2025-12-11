08fc796d71e947be99e646b4ba86f70b40b454a4
HoaNgx-179 <hoahoa0917@gmail.com>
2025-12-11 08:54:04 +0700

    Update index.js

--- backend/matchservice/index.js
+++ backend/matchservice/index.js
@@ -7,11 +7,14 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-// Sử dụng route đúng
 app.use("/api", matchRoutes);
 
 const PORT = process.env.PORT || 8006;
 
 app.listen(PORT, () => {
   console.log(`Match Service running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Match Service is running!");
 });
