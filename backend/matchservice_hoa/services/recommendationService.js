7ab5ef203f3f222d5f5609fc04b45154fa98db86
HoaNgx-179 <hoahoa0917@gmail.com>
2025-12-15 08:30:40 +0700

    Update recommendationService.js

--- backend/matchservice_hoa/services/recommendationService.js
+++ backend/matchservice_hoa/services/recommendationService.js
@@ -3,39 +3,65 @@ import { pool } from "../config/db.js";
 // Lấy user chưa swipe
 export const fetchNextUsers = async (userId) => {
   const query = `
-    SELECT
-      u.user_id,
-      u.username,
-      p.bio,
-      p.job_title,
-      p.company,
-      p.education,
-      p.gender,
-      p.birth_date,
-      ph.url AS avatar,
-      ARRAY_AGG(DISTINCT im.interest_tag)
-        FILTER (WHERE im.interest_tag IS NOT NULL) AS interests
-    FROM users u
-    JOIN profiles p ON u.user_id = p.user_id
-    LEFT JOIN photos ph
-      ON u.user_id = ph.user_id AND ph.is_primary = true
-    LEFT JOIN user_interests ui ON u.user_id = ui.user_id
-    LEFT JOIN interest_map im ON ui.interest_id = im.interest_id
-    WHERE u.user_id != $1
-      AND p.is_active = true
-      AND NOT EXISTS (
-        SELECT 1
-        FROM swipes s
-        WHERE s.from_user_id = $1
-          AND s.to_user_id = u.user_id
-      )
-    GROUP BY u.user_id, p.user_id, ph.url
+    SELECT *
+    FROM (
+      SELECT
+        u.user_id,
+        u.username,
+        p.bio,
+        p.job_title,
+        p.company,
+        p.education,
+        p.gender,
+        p.birth_date,
+        ph.url AS avatar,
+
+        ROUND(
+          (ST_Distance(
+            p.location::geography,
+            me.location::geography
+          ) / 1000)::numeric,
+          1
+        ) AS distance_km,
+
+        ARRAY_AGG(DISTINCT im.interest_tag)
+          FILTER (WHERE im.interest_tag IS NOT NULL) AS interests
+
+      FROM users u
+      JOIN profiles p ON u.user_id = p.user_id
+      JOIN profiles me ON me.user_id = $1
+
+      LEFT JOIN photos ph
+        ON u.user_id = ph.user_id AND ph.is_primary = true
+      LEFT JOIN user_interests ui ON u.user_id = ui.user_id
+      LEFT JOIN interest_map im ON ui.interest_id = im.interest_id
+
+      WHERE u.user_id != $1
+        AND p.is_active = true
+        AND p.location IS NOT NULL
+        AND me.location IS NOT NULL
+        AND NOT EXISTS (
+          SELECT 1
+          FROM swipes s
+          WHERE s.from_user_id = $1
+            AND s.to_user_id = u.user_id
+        )
+
+      GROUP BY
+        u.user_id,
+        p.user_id,
+        ph.url,
+        p.location,
+        me.location
+    ) t
+    ORDER BY t.distance_km ASC
     LIMIT 10;
   `;
 
   const result = await pool.query(query, [userId]);
   return result.rows;
 };
+
 
 export const recordSwipe = async (fromUser, toUser, action) => {
   // 1. Lưu swipe
